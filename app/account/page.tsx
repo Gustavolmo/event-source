07d044b6e543950c324fde23369b672262b8c165
Gustavo L.M. de Oliveira <lmo.gustavo@gmail.com>
2023-08-16 22:03:08 +0200

    (fix) no zoom on mobile

--- app/account/page.tsx
+++ app/account/page.tsx
@@ -1,4 +1,5 @@
 'use server';
+import Head from 'next/head'; // FIX ZOOM ON MOBILE?
 import React, { Suspense } from 'react';
 import AccountPage from '../components/accountComponents/AccountPage';
 import Loading from '../components/Loading';
@@ -8,6 +9,12 @@ export default async function MyAccount() {
   return (
     <>
       <Suspense fallback={<Loading />}>
+        <Head>
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"
+          />
+        </Head>
         <Navbar path={'/'} title={'Home'} className={'navbar__button'} />
         <main className="main">
           <AccountPage />
