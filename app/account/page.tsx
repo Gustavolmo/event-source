1206db590c4cb4488074eab410b560c5d241a653
Gustavo L.M. de Oliveira <lmo.gustavo@gmail.com>
2023-08-16 22:23:52 +0200

    (fix) no zoom on mobile

--- app/account/page.tsx
+++ app/account/page.tsx
@@ -3,10 +3,14 @@ import React, { Suspense } from 'react';
 import AccountPage from '../components/accountComponents/AccountPage';
 import Loading from '../components/Loading';
 import Navbar from '../components/Navbar';
+import Head from 'next/head';
 
 export default async function MyAccount() {
   return (
     <>
+      <Head>
+        <meta name="viewport" content="width=device-width, user-scalable=no" />
+      </Head>
       <Suspense fallback={<Loading />}>
         <Navbar path={'/'} title={'Home'} className={'navbar__button'} />
         <main className="main">
