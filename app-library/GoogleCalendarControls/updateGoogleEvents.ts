824ea2663da44a6cc0f6a54506301b9c90097445
Gustavo L.M. de Oliveira <lmo.gustavo@gmail.com>
2023-08-25 20:09:12 +0200

    (refactor) updateGoogleEvents logic into own functions

--- app-library/GoogleCalendarControls/updateGoogleEvents.ts
+++ app-library/GoogleCalendarControls/updateGoogleEvents.ts
@@ -5,6 +5,7 @@ import {
   addGuestToListController,
   removeGuestFromList,
 } from '../InvitationControls';
+import { ObjectId } from 'mongodb';
 
 export const getGoogleEventUpdate = async (
   accessToken: string,
@@ -27,25 +28,46 @@ export const getGoogleEventUpdate = async (
       console.log('Data retrieved successfully');
       const calendarData: GoogleEventResponse = await googleRes.json();
 
-      calendarData.attendees.forEach((guest) => {
-        if (guest.responseStatus === 'accepted') {
-          addGuestToListController(guest.email, event._id, 'acceptedLive');
-        }
-        if (guest.responseStatus === 'tentative') {
-          addGuestToListController(guest.email, event._id, 'maybeAccepted');
-        }
-        if (guest.responseStatus === 'declined') {
-          addGuestToListController(guest.email, event._id, 'rejected');
-        }
-        if (guest.responseStatus === 'needsAction') {
-          removeGuestFromList(guest.email, event._id, 'acceptedLive');
-          removeGuestFromList(guest.email, event._id, 'maybeAccepted');
-          removeGuestFromList(guest.email, event._id, 'rejected');
-        }
-      });
+      filterAttendence(calendarData, event)
+
+      // calendarData.attendees.forEach((guest) => {
+      //   if (guest.responseStatus === 'accepted') {
+      //     addGuestToListController(guest.email, event._id, 'acceptedLive');
+      //   }
+      //   if (guest.responseStatus === 'tentative') {
+      //     addGuestToListController(guest.email, event._id, 'maybeAccepted');
+      //   }
+      //   if (guest.responseStatus === 'declined') {
+      //     addGuestToListController(guest.email, event._id, 'rejected');
+      //   }
+      //   if (guest.responseStatus === 'needsAction') {
+      //     removeGuestFromList(guest.email, event._id, 'acceptedLive');
+      //     removeGuestFromList(guest.email, event._id, 'maybeAccepted');
+      //     removeGuestFromList(guest.email, event._id, 'rejected');
+      //   }
+      // });
     } else {
       console.error('Failed to get data', googleRes);
       return false;
     }
   });
 };
+
+const filterAttendence = (calendarData: GoogleEventResponse, event: EventData) => {
+  calendarData.attendees.forEach((guest) => {
+    if (guest.responseStatus === 'accepted') {
+      addGuestToListController(guest.email, event._id, 'acceptedLive');
+    }
+    if (guest.responseStatus === 'tentative') {
+      addGuestToListController(guest.email, event._id, 'maybeAccepted');
+    }
+    if (guest.responseStatus === 'declined') {
+      addGuestToListController(guest.email, event._id, 'rejected');
+    }
+    if (guest.responseStatus === 'needsAction') {
+      removeGuestFromList(guest.email, event._id, 'acceptedLive');
+      removeGuestFromList(guest.email, event._id, 'maybeAccepted');
+      removeGuestFromList(guest.email, event._id, 'rejected');
+    }
+  });
+}
